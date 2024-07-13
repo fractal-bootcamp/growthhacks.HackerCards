571ca9c3d78015104f17d1047fc24d901d7eb9df
Brian Smiley <briantsmiley42@gmail.com>
2024-07-13 16:07:20 -0400

    adds filetype svgs

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -6,23 +6,31 @@ interface CardProps {
     hp: number;
     attack: number;
     defense: number;
+    svgPath: string;
   };
 }
-
 const Card = ({ data }: CardProps) => {
-  const { name, imageUrl, type, hp, attack, defense } = data;
+  const { name, imageUrl, type, hp, attack, defense, svgPath } = data;
+  console.log(svgPath);
   return (
     <div className="bg-yellow-400 w-[350px] h-[490px] rounded-xl m-2 p-3 flex justify-center items-center">
       <div className="bg-slate-400 w-full h-full  flex flex-col items-center">
-        <div className="flex flex-col items-center w-full p-5">
-          <div className="flex justify-between w-full items-end">
+        <div className="flex flex-col items-center w-full p-5 gap-1">
+          <div className="flex justify-between w-full h-[20px] items-end">
             <div className="font-bold text-xl">{name}</div>
-            <p
-              className=" text-gray-700 text-base tooltip-top tooltip z-20 "
-              data-tip="Hacker Points"
-            >
-              {hp} HP
-            </p>
+            <div className="flex h-full gap-1 items-center">
+              <div
+                className=" text-gray-700 text-base tooltip-top tooltip z-20 flex"
+                data-tip="Hacker Points"
+              >
+                {hp} HP
+              </div>
+              <img
+                className="h-full aspect-square object-cover bg-slate-200 rounded-full"
+                src={svgPath}
+                alt="file type"
+              />
+            </div>
           </div>
           <div className="bg-gradient-to-br from-yellow-400 via-yellow-700 to-yellow-400 p-2 w-full aspect-[5/4] object-cover overflow-hidden relative ">
             <img
