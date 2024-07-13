4b242dffffb3011e534d13cb77cf9d8077daf720
Brian Smiley <briantsmiley42@gmail.com>
2024-07-13 14:39:59 -0400

    adds daisyui tooltips

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -17,9 +17,14 @@ const Card = ({ data }: CardProps) => {
         <div className="flex flex-col items-center w-full p-5">
           <div className="flex justify-between w-full items-end">
             <div className="font-bold text-xl">{name}</div>
-            <p className="text-gray-700 text-base">{hp} HP</p>
+            <p
+              className=" text-gray-700 text-base tooltip-top tooltip z-20 "
+              data-tip="Hacker Points"
+            >
+              {hp} HP
+            </p>
           </div>
-          <div className="bg-gradient-to-br from-yellow-400 via-yellow-700 to-yellow-400 p-2 w-full aspect-[4/3] object-cover overflow-hidden relative ">
+          <div className="bg-gradient-to-br from-yellow-400 via-yellow-700 to-yellow-400 p-2 w-full aspect-[5/4] object-cover overflow-hidden relative ">
             <img
               className="relative inset-0 z-10 object-cover h-full w-full"
               src={imageUrl}
